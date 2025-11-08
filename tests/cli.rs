8ca792b6f16158fca7fcbb00bd53eecf3a8ca0c3
Charles Lechasseur <shiftingbeard@outlook.com>
2025-11-08 18:52:26 -0500

    chore: replace deprecated API from `assert_cmd`

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,9 +1,9 @@
-use assert_cmd::{crate_name, Command};
+use assert_cmd::{cargo_bin, Command};
 use test_log::test;
 
 #[test]
 fn test_backup_basic() {
-    let mut cmd = Command::cargo_bin(crate_name!()).unwrap();
+    let mut cmd = Command::new(cargo_bin!());
 
     cmd.arg("backup").arg("--help").assert().success();
 }
